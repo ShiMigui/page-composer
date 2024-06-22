784c9f1a47805b790a3bdf7a9efa7365f77d6324
shimigui <miguel.nasto@gmail.com>
2024-06-22 19:08:09 -0300

    feat: add try-catch to function

--- composer/utils/loadDom.js
+++ composer/utils/loadDom.js
@@ -3,10 +3,16 @@ const fs = require('fs');
 
 module.exports = (log = () => null) => {
     return function loadDom(path = '') {
-        const content = fs.readFileSync(path, { encoding: 'utf-8' });
-        const DOM = new JSDOM(content);
-        log(`[loadDom.js] DOM is loaded: path: ${path}`);
-        
-        return DOM;
+        try {
+            const content = fs.readFileSync(path, { encoding: 'utf-8' });
+            const DOM = new JSDOM(content);
+            log(`[loadDom.js] DOM was loaded: path: ${path}`);
+
+            return DOM;
+        }
+        catch (e) {
+            log(`[loadDom.js] DOM cannot be loaded: path: ${path}, error: ${e.message}`);
+            throw e;
+        }
     }
 }
